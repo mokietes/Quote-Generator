beef873bf6a27eb9856592df6c3e163ad70d52cb
abc <abc@example.com>
2022-04-13 19:43:52 -0700

    update

--- js.js
+++ js.js
@@ -1,22 +1,45 @@
+const twitterButton = document.querySelector("#js-tweet");
+
+//const rec = document.getElementsByClassName("rect1 rect2");
+const spinner = document.querySelector("#js-spinner");
 const newQuoteButton = document.querySelector("#js-new-quote");
+//rec3.style.backgroundColor = "white";
 newQuoteButton.addEventListener("click", getQuote);
 
 const endpoint = "https://api.whatdoestrumpthink.com/api/v1/quotes/random";
 
 async function getQuote() {
+  //spinner.classList.add("spinner hidden");
+  //spinner.classList.remove("hidden");
+  newQuoteButton.disabled = true;
+
   try {
+    spinner.style.display = "block";
+    //spinner.classList.remove("hidden");
+
     const response = await fetch(endpoint);
     if (!response.ok) {
       throw Error(response.statusText);
     }
+
     const json = await response.json();
     displayQuote(json.message);
-  } catch (err) {
-    console.log(err);
+  } catch {
     alert("Failed to fetch new quote");
+  } finally {
+    newQuoteButton.disabled = false;
+    spinner.style.display = "none";
+    //spinner.classList.add("hidden");
   }
 }
 
 function displayQuote(quote) {
-  document.querySelector("#js-quote-text").textContent = quote;
+  const quoteText = document.querySelector("#js-quote-text");
+  quoteText.textContent = quote;
+}
+function setTweetButton(quote) {
+  twitterButton.setAttribute(
+    "href",
+    "https://twitter.com/share?text=${quote} - Donald Trump"
+  );
 }
